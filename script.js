53a1e26ba5dbeb3459e30da00345e03c757ff259
gigigouvea27 <166717992+gigigouvea27@users.noreply.github.com>
2026-06-30 11:14:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Seleção dos elementos do DOM
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+// Estrutura de dados das perguntas
+const perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+];
